ef952559b9a97e420b51b3abb8404adba825d76f
Santiago Dobarro <santiagodobarro@gmail.com>
2023-09-08 18:06:56 -0300

    Proyecto Final

---
+++ JS/app.js
@@ -0,0 +1,44 @@
+document.getElementById('convert').addEventListener('click', function() {
+    const monto = parseFloat(document.getElementById('monto').value);
+
+if (isNaN(monto)) {
+    Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Ingresar un monto para que sea convertido',
+        showConfirmButton: false,
+        timer: 800
+})
+        document.getElementById('result').textContent = '';
+        return;
+    }
+
+    // fetch('./exchangeRates.json')
+    const getDivisas = async () => {
+        
+    const response = await fetch('')
+    const divisas = await response.json()
+    localData(divisas)
+    // .then(response => response.json())
+    // .then(data => {
+        const usd = monto / data.USD;
+        const eur = monto / data.EUR;
+        const brl = monto / data.BRL;
+        
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Convirtiendo valor',
+            showConfirmButton: false,
+            timer: 800
+        })
+        document.getElementById('result').innerHTML = `
+        <p>${monto} Pesos Argentinos equivale a:</p>
+        <p>${usd.toFixed(2)} Dólares</p>
+        <p>${eur.toFixed(2)} Euros</p>
+        <p>${brl.toFixed(2)} Real Brasileño </p>
+    }
+    }
+    `;   
+}
+})
